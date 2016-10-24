fa47039435d7f18203161a6edf14d20e6f48179c
Anirudh Vemula <vvanirudh@gmail.com>
2016-10-24 12:14:43 -0400

    Added comments to the Utils script

--- utils.py
+++ utils.py
@@ -15,23 +15,32 @@ class DataLoader():
         seq_length : RNN sequence length
         '''
         # List of data directories where raw data resides
-        self.data_dirs = ['./data/eth/univ', './data/eth/hotel',
-                          './data/ucy/zara/zara01', './data/ucy/zara/zara02',
-                          './data/ucy/univ']
+        # self.data_dirs = ['./data/eth/univ', './data/eth/hotel',
+        #                  './data/ucy/zara/zara01', './data/ucy/zara/zara02',
+        #                  './data/ucy/univ']
+
+        # self.data_dirs = ['./data/eth/univ', './data/eth/hotel']
+        self.data_dirs = ['./data/eth/univ']
 
         # Data directory where the pre-processed pickle file resides
         self.data_dir = './data'
 
+        # Store the batch size and the sequence length arguments
         self.batch_size = batch_size
         self.seq_length = seq_length
 
+        # Define the path of the file in which the data needs to be stored
         data_file = os.path.join(self.data_dir, "trajectories.cpkl")
 
+        # If the file doesn't exist already or if forcePreProcess is true
         if not(os.path.exists(data_file)) or forcePreProcess:
             print("Creating pre-processed data from raw data")
+            # Preprocess the data from the csv files
             self.preprocess(self.data_dirs, data_file)
 
+        # Load the data from the pickled file
         self.load_preprocessed(data_file)
+        # Reset all the pointers
         self.reset_batch_pointer()
 
     def preprocess(self, data_dirs, data_file):
@@ -50,9 +59,12 @@ class DataLoader():
         all_ped_data = {}
         dataset_indices = []
         current_ped = 0
+        # For each dataset
         for directory in data_dirs:
+            # Define the path to its respective csv file
             file_path = os.path.join(directory, 'pixel_pos.csv')
 
+            # Load data from the csv file
             # Data is a 4 x numTrajPoints matrix
             # where each column is a (frameId, pedId, y, x) vector
             data = np.genfromtxt(file_path, delimiter=',')
@@ -60,6 +72,7 @@ class DataLoader():
             # Get the number of pedestrians in the current dataset
             numPeds = np.size(np.unique(data[1, :]))
 
+            # For each pedestrian in the dataset
             for ped in range(1, numPeds+1):
                 # Extract trajectory of the current ped
                 traj = data[:, data[1, :] == ped]
@@ -73,7 +86,9 @@ class DataLoader():
             dataset_indices.append(current_ped+numPeds)
             current_ped += numPeds
 
+        # The complete data is a tuple of all pedestrian data, and dataset ped indices
         complete_data = (all_ped_data, dataset_indices)
+        # Store the complete data into the pickle file
         f = open(data_file, "wb")
         pickle.dump(complete_data, f, protocol=2)
         f.close()
@@ -89,33 +104,45 @@ class DataLoader():
         self.raw_data = pickle.load(f)
         f.close()
 
+        # Get the pedestrian data from the pickle file
         all_ped_data = self.raw_data[0]
         # Not using dataset_indices for now
         # dataset_indices = self.raw_data[1]
 
+        # Construct the data with sequences(or trajectories) longer than seq_length
         self.data = []
         counter = 0
 
+        # For each pedestrian in the data
         for ped in all_ped_data:
+            # Extract his trajectory
             traj = all_ped_data[ped]
+            # If the length of the trajectory is greater than seq_length (+2 as we need both source and target data)
             if traj.shape[1] > (self.seq_length+2):
                 # TODO: (Improve) Store only the (x,y) coordinates for now
                 self.data.append(traj[[0, 1], :].T)
                 # Number of batches this datapoint is worth
                 counter += int(traj.shape[1] / ((self.seq_length+2)))
 
+        # Calculate the number of batches (each of batch_size) in the data
         self.num_batches = int(counter / self.batch_size)
 
     def next_batch(self):
         '''
         Function to get the next batch of points
         '''
+        # List of source and target data for the current batch
         x_batch = []
         y_batch = []
+        # For each sequence in the batch
         for i in range(self.batch_size):
+            # Extract the trajectory of the pedestrian pointed out by self.pointer
             traj = self.data[self.pointer]
+            # Number of sequences corresponding to his trajectory
             n_batch = int(traj.shape[0] / (self.seq_length+2))
+            # Randomly sample a index from which his trajectory is to be considered
             idx = random.randint(0, traj.shape[0] - self.seq_length - 2)
+            # Append the trajectory from idx until seq_length into source and target data
             x_batch.append(np.copy(traj[idx:idx+self.seq_length, :]))
             y_batch.append(np.copy(traj[idx+1:idx+self.seq_length+1, :]))
 
@@ -136,4 +163,7 @@ class DataLoader():
             self.pointer = 0
 
     def reset_batch_pointer(self):
+        '''
+        Reset the data pointer
+        '''
         self.pointer = 0
